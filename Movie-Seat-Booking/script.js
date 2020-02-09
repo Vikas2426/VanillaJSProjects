18e101cf02fb91a167bed45568d1d6d03ddfcac9
Vikas Choubey <vikas.cby@gmail.com>
2020-02-09 17:12:07 +0530

    Added localStorage functionality

--- Movie-Seat-Booking/script.js
+++ Movie-Seat-Booking/script.js
@@ -5,16 +5,28 @@ const total = document.querySelector(".total");
 const price = document.querySelector("#price");
 const movie = document.querySelector("#movie");
 const cost = document.querySelector("#cost");
+
 let ticketPrice = +movie.value;
 cost.innerText = ticketPrice;
+
+//Store in local Storage
+const storeMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("movieIndex", movieIndex);
+  localStorage.setItem("moviePrice", moviePrice);
+};
 
 //Update seat count and total price
 const updateTotal = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
-  const seats = selectedSeats.length;
+  const seatCount = selectedSeats.length;
+  count.innerText = seatCount;
+  price.innerText = seatCount * ticketPrice;
 
-  count.innerText = seats;
-  price.innerText = seats * ticketPrice;
+  //Store in Local Storage
+  const selectedSeatsIndex = [...selectedSeats].map(seat =>
+    [...seats].indexOf(seat)
+  );
+  localStorage.setItem("selectedSeats", JSON.stringify(selectedSeatsIndex));
 };
 
 //Seat Click Event
@@ -34,4 +46,39 @@ movie.addEventListener("change", e => {
   ticketPrice = +e.target.value;
   cost.innerText = ticketPrice;
   updateTotal();
+
+  //Store in Local Storage
+  storeMovieData(e.target.selectedIndex, e.target.value);
 });
+
+//Populate UI with locally stored data
+const populateUI = () => {
+  // populate selected seats
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      //index is present in selectedSeats
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  //populate selected movie
+  const selectedMovieIndex = localStorage.getItem("movieIndex");
+
+  if (selectedMovieIndex != null) {
+    movie.selectedIndex = selectedMovieIndex;
+  }
+
+  //populate selected movie price
+  const selectedMoviePrice = localStorage.getItem("moviePrice");
+  if (selectedMoviePrice != null) {
+    ticketPrice = +selectedMoviePrice;
+    cost.innerText = ticketPrice;
+    updateTotal();
+  }
+};
+
+populateUI();
